6f10b4709ee90bfcfdecb242bdf39c3d5302bbcf
NatannyR <natannyrezende.silva@gmail.com>
2025-08-11 15:15:43 +1000

    INCLUSAO DE CONST NO JS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
 const adicionar = () => {
     const novaTarefa = document.getElementById("novaTarefa").value;
 
+    if (novaTarefa.trim() === "") {
+        alert("Por favor, digite uma tarefa válida.");
+        return;
+    }
+
+    const listaTarefas = document.getElementById("listaTarefas");
+
+    const item = document.createElement("li");
+    const textoTarefa = document.createTextNode(novaTarefa);
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "Excluir";
+    botaoExcluir.className = "delete-btn";
+
+    botaoExcluir.addEventListener("click", function() {
+        listaTarefas.removeChild(item);
+    }   
+
+    item.addEventListener   ("click", function() {
+        item.classList.toggle("completed");
+    });
+
     
