7bcf7a5d5de3e43ae617de651147a6827b215424
NatannyR <natannyrezende.silva@gmail.com>
2025-08-15 17:19:59 +1000

    add botao excluir na lista de tarefas

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
-    const adicionar = document.getElementById("adicionarbtn");
-    adicionar.addEventListener("click", () => {
-        const input = document.getElementById("novatarefa");
-        const texto = input.value;
-        const lista = document.getElementById("listatarefas");
+const adicionar = document.getElementById("adicionarbtn");
+adicionar.addEventListener("click", () => {
+  const input = document.getElementById("novatarefa");
+  const texto = input.value;
+  const lista = document.getElementById("listatarefas");
 
-    if (texto.trim() === "") {
-        alert("insira alguma coisa");
-        return;
+  if (texto.trim() === "") {
+    alert("insira alguma coisa");
+    return;
+  } else {
+    const novoItem = document.createElement("li");
+    const textoSpan = document.createElement("span");
+    textoSpan.textContent = texto;
 
-    } else {
-        const novoItem = document.createElement("li");
-        novoItem.textContent = texto;
-        lista.appendChild(novoItem);
-        input.value = "";
-    }
-    });
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "Excluir";
 
+    botaoExcluir.onclick = function () {
+      novoItem.remove();
+    };
 
+    novoItem.appendChild(textoSpan);
+    novoItem.appendChild(botaoExcluir);
+    lista.appendChild(novoItem);
+    input.value = "";
+  }
+});
